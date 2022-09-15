aff0c7ac14eb0dd9ab9300b77d0717e7a3dcf560
Dominik Sust <dominik.sust@googlemail.com>
2022-09-15 13:01:07 +0200

    Removed SetTimestampFormat function

--- configuration.go
+++ configuration.go
@@ -1,14 +1,8 @@
 package gologger
 
 var loglevel Level = DebugLevel
-var timestampFormat = "2006-01-02 15:04:05.000"
 
 // Set the loglevel
 func SetLogLevel(level Level) {
 	loglevel = level
-}
-
-// Set the timestamp format (see time.Format)
-func SetTimestampFormat(format string) {
-	timestampFormat = format
 }
