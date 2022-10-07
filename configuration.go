8921eee9ef4b8dbdca7070b587fa99242e03c5b9
Dominik Sust <dominik.sust@buerotex.de>
2022-10-07 16:02:38 +0200

    Added func SetTimestampFormat

--- configuration.go
+++ configuration.go
@@ -1,8 +1,19 @@
 package gologger
 
+import stdlog "log"
+
 var loglevel Level = DebugLevel
+
+var timestampFormat = "2006-01-02 15:04:05.000"
 
 // Set the loglevel
 func SetLogLevel(level Level) {
 	loglevel = level
+}
+
+// Set the timestamp format (see time.Format)
+// and sets flags of log package to 0
+func SetTimestampFormat(format string) {
+	stdlog.SetFlags(0)
+	timestampFormat = format
 }
